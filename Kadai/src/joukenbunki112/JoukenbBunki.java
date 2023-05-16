771134a3b1badb5b15b69f248d7d01ee3f17164b
Nozomi-Hirose <g.road.hrs.n@gmail.com>
2023-05-16 19:06:55 +0900

    提出1-12

--- Kadai/src/joukenbunki112/JoukenbBunki.java
+++ Kadai/src/joukenbunki112/JoukenbBunki.java
@@ -7,9 +7,9 @@ public class JoukenbBunki {
 		System.out.println("問１");
 		//問題1:下記の条件を満たすようにif文を3つネストして条件式を作成してください。
 		//
-		//変数x,y,zは正の整数であれば下の条件に続く。それ以外は”正の整数：NG”と表示する
-		//xの値が10の倍数であれば下の条件に続く。それ以外は”10の倍数：NG”と表示する
-		//x + y + z = 100であれば”プラス100：OK”それ以外は”プラス100：NG”と表示する
+		//変数x,y,zは正の整数であれば★下の条件に続く。（それ以外は”正の整数：NG”と表示する）
+		//xの値が10の倍数であれば下の条件に続く。（それ以外は”10の倍数：NG”と表示する）
+		//x + y + z = 100であれば”プラス100：OK”（それ以外は”プラス100：NG”と表示する）
 		
 		int x = 10;
 		int y = 80;
@@ -26,21 +26,26 @@ public class JoukenbBunki {
 				 * x + y + z == 100　"プラス100：NG"*/
 				if(x + y + z == 100) {
 					System.out.println("プラス100：OK"); 
-				} else {
+					} else {
 					System.out.println("プラス100：NG");
-					}
-			
-				/*★↓条件式の中に条件式）*/
+					} 
+
+		
+				/*★↓条件式の中に条件式）
 				if (x >=0 && y>=0 && z>=0 &&(x % 10 == 0)) {
 					if(x + y + z == 100) {
 						System.out.println("プラス100：OK"); 
 					} else {
 						System.out.println("プラス100：NG");
 					} 
-				}
+				}*/
+			}else {
+				System.out.println("10の倍数：NG");
 			}
 			
 
+		}else {
+			System.out.println("正の整数：NG");
 		}
 
 		/*★↑全て条件を満たすときと条件３だけ満たさないときは上記で良いかと思うのですが、
@@ -51,8 +56,12 @@ public class JoukenbBunki {
 		//改行
 		System.out.println("問２");
 		//問題2:問1と同じ内容で三項演算子で条件式を作成しなさい。最後に変数aを出力する。
-		//String a = (x >=0 && y>=0 && z>=0)?"";
-	}
+		String a = (x >=0 && y>=0 && z>=0) ? ((x % 10 == 0) ? ((x + y + z == 100) ? "プラス100：OK" : "プラス100：NG") : "10の倍数：NG") : "正の整数：NG";
+		System.out.println(a);
+		
+		
 }
+}
+
 
 
