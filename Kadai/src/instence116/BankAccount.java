6aca6afa75f41373d3a2cc6826ac0885d6926fc4
Nozomi-Hirose <g.road.hrs.n@gmail.com>
2023-06-08 23:22:21 +0900

    提出1-16

--- Kadai/src/instence116/BankAccount.java
+++ Kadai/src/instence116/BankAccount.java
@@ -3,46 +3,43 @@ package instence116;
 
 //下記BankAccount.java
 public class BankAccount {
-	
+
 	/*BankAccountクラスに何の情報を渡すか？
 	 * */
 	private String accountNumber;
 	private double balance;
-	
+
 	//問１のTest1.javaの"山田"、"100000.0"がそれぞれセットされる
-	public BankAccount(String accountNumber, double balance) {
+	public BankAccount(String accountNumber, double balance) { //publicのメソッドだから他のクラスでも使える？
 
 
 		//アカウント名
 		this.accountNumber = accountNumber;
 		//初期残高
-		this.balance = balance;
+		this.balance = balance; //メンバ変数・ローカル変数を区別するため。ここで定義して
+	
+	} 
+
+	//残高照会のメソッド定義
+	public double getBalance() { //値を入れる箱（中身を見たいだけなので直接値は入れない）
+		return balance; //
 	}
 
 
-	public double getBalance() {
-		return balance;
-	}
-
-
-	//残高
+	//預入れ処理のメソッド定義
 	public void deposit(double amount) {
 		if (amount > 0) {
 			balance += amount;
 		}
-
-
 	}
+	
+	//引落処理のメソッド定義
 	public void withdraw(double amount) {
 		if (amount > 0 && balance >= amount) {
 			balance -= amount;
 		}
 
 	}
-
-
-	public void amount(double d) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
 }
+
+//コンストラクタ…インスタンスを生成するときに実行される機能のこと
--- Kadai/src/instence116/Test1.java
+++ Kadai/src/instence116/Test1.java
@@ -5,35 +5,63 @@ public class Test1 {
 		//改行
 		System.out.println("問１");
 		//問題1:新しいBankAccountインスタンス：「yamada」を作成し、名前(山田)と初期残高(10万円)を設定し「残高」を出力してください。
-		BankAccount yamada = new BankAccount("山田",100000.0); //BAckAccountをインスタンス化(accountNumberに山田、
-		//yamada.accountNumber = "yamada";
-		System.out.println(yamada.getBalance());
 
+		//使用したいクラス名・使いたいファイルの中での変数名　＝　new演算子　（コンストラクタ）←インスタンス化したときに動くやつ
+		BankAccount yamada = new BankAccount("山田",100000); //BAckAccountをインスタンス化(accountNumberに山田、
+		//		//【メモ】newで新しいインスタンス(yamada）を作成
+		//		//これでyamadaに預金情報を持たせることができる
+		//		
+		//		
+		System.out.println(yamada.getBalance());
 		
-		
+		double balance = yamada.getBalance();
+		System.out.println(balance);
+		//		【メモ】
+		//		public BankAccount(String accountNumber, double balance) {
+		//
+		//
+		//			//アカウント名
+		//			this.accountNumber = accountNumber;
+		//			//初期残高
+		//			this.balance = balance; ★←ここにdouble balanceの値が入る★
+		//		}
+		//		
+		//　★	public double getBalance() {
+		//		return balance; ★←BankAccountの引数double balanceの値をそのまま返すメソッド
+		//	}
+
+
 		//改行
 		System.out.println("問２");
 		//問題2:BankAccountインスタンス「yamada」の残高3万円を預金して「残高」を出力してください。
-//		yamada.amount(30000.0);
-//		BankAccount.deposit();
-//		
-//		System.out.println(deposit);
-		
+		yamada.deposit(30000); //インスタンスyamadaにBankAccountクラスのdeposit()メソッドを使って30000円預金する。yamad.〇
+		System.out.println(yamada.getBalance());
+
+
 		//改行
 		System.out.println("問３");
 		//問題3:BankAccountインスタンス「yamada」の残高より8万円引き落として「残高」を出力してください。
+		yamada.withdraw(80000);
+		System.out.println(yamada.getBalance());
+
+
 		//改行
 		System.out.println("問４");
 		//問題4:新しいPersonインスタンス：「sato」を作成し、名前(佐藤)、年齢(25)、性別(男性)を設定し「性別」を出力してください。
 		Person sato = new Person("佐藤", 25, "男性");
 		System.out.println(sato.getGender());
-		
+
 		//改行
 		System.out.println("問５");
 		//問題5:Personインスタンス：「sato」の年齢を変更しようとして「無効な年齢です１」と出力させてください。
+		sato.setAge(-1000);
+
 		//改行
 		System.out.println("問６");
 		//問題6:Personインスタンス：「sato」の性別を「男性」→「女性」に変更して性別を出力してください。
+
+		sato.setGender("女性");
+		System.out.println(sato.getGender());
 	}
 
 }
